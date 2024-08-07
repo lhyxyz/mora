58d265df0ec821f9cafe24d05a21aeb7e011cf5b
Windy <548343523@qq.com>
2024-08-07 09:51:23 +0800

    add primogem, stellar and polychrome. Update recipes

--- src/main/java/com/mora/item/itemGroup.java
+++ src/main/java/com/mora/item/itemGroup.java
@@ -13,12 +13,15 @@ public class itemGroup {
     public static final ItemGroup SILVER_GROUP = Registry.register(Registries.ITEM_GROUP,
             new Identifier(Mora.MOD_ID, "mora"),
             FabricItemGroup.builder().displayName(Text.translatable("itemGroup.mora"))
-                    .icon(() -> new ItemStack(items.MORA)).entries((displayContext, entries) -> {
-                        entries.add(items.MORA);
+                    .icon(() -> new ItemStack(items.mora)).entries((displayContext, entries) -> {
+                        entries.add(items.mora);
+                        entries.add(items.primogem);
                         entries.add(items.intertwined_fate);
                         entries.add(items.credit);
+                        entries.add(items.stellar);
                         entries.add(items.pass);
                         entries.add(items.denny);
+                        entries.add(items.polychrome);
                         entries.add(items.tape);
                     }).build());
 
--- src/main/java/com/mora/item/items.java
+++ src/main/java/com/mora/item/items.java
@@ -8,12 +8,21 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class items {
-    public static final Item MORA = regItem("mora", new Item(new FabricItemSettings()));
+    /* Genshin Impact */
+    public static final Item mora = regItem("mora", new Item(new FabricItemSettings()));
+    public static final Item primogem = regItem("primogem", new Item(new FabricItemSettings()));
     public static final Item intertwined_fate = regItem("intertwined_fate", new Item(new FabricItemSettings()));
+
+    /* Star Rail */
     public static final Item credit = regItem("credit", new Item(new FabricItemSettings()));
+    public static final Item stellar = regItem("stellar", new Item(new FabricItemSettings()));
     public static final Item pass = regItem("pass", new Item(new FabricItemSettings()));
+
+    /* Zenless Zone Zero */
     public static final Item denny = regItem("denny", new Item(new FabricItemSettings()));
+    public static final Item polychrome = regItem("polychrome", new Item(new FabricItemSettings()));
     public static final Item tape = regItem("tape", new Item(new FabricItemSettings()));
+
 
     private static Item regItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(Mora.MOD_ID, name), item);
